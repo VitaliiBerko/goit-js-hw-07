8b60c4f30fca97a1f35ff8e20be6c538eae4cd04
Vitalii Berko <vitalii.v.berko@gmail.com>
2022-10-10 16:12:48 +0300

    Added task1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,48 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryConteiner = document.querySelector('.gallery');
+const cardsMarkUp = createPhotoCartMarkup(galleryItems);
+
+galleryConteiner.insertAdjacentHTML('beforeend', cardsMarkUp);
+
+galleryConteiner.addEventListener('click', onPaletContainerClick);
+
+function createPhotoCartMarkup(photos) {
+    return galleryItems.map( ({preview, original, description}) => {
+        return ` <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </div>`
+    }).join('');     
+}
+
+function onPaletContainerClick(event) {
+    event.preventDefault();
+
+    const isPhotoCardEl = event.target.classList.contains('gallery__image');
+
+    if(!isPhotoCardEl) {
+        return;
+    }
+
+    // console.log(event.target.dataset.source);
+    
+   const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}">
+`)
+
+instance.show()
+   
+}
+
+
+
+
